a6373c4ae11336257ec12236ba37f0d864d24edb
Rohit Kalyan KR <mail.rohitkalyan@gmail.com>
2023-02-28 20:07:58 +0530

    Display method added

--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -1,5 +1,14 @@
 package LinkedLists;
 
 public class Main {
-    linkedList list = new linkedList();
+
+    public static void main(String[] args) {
+        linkedList list = new linkedList();
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(6);
+        list.insertFirst(7);
+        list.display();
+    }
+
 }
--- LinkedLists/linkedList.java
+++ LinkedLists/linkedList.java
@@ -4,25 +4,42 @@ public class linkedList {
     private Node head;
     private Node tail;
     int size;
-    linkedList(){
-        this.size=0;
+
+    linkedList() {
+        this.size = 0;
     }
-    public void insertFirst(int val){
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value+"->");
+            temp = temp.next;
+        }
+        System.out.println("End");
+    }
+
+    public void insertFirst(int val) {
         Node node = new Node(val);
         node.next = head;
-
+        head = node;
+        if(tail == null){
+            tail = head;
+        }
+        size++;
     }
-    private class Node{
+
+    class Node {
         int value;
         Node next;
 
-        public Node(int value){
-            this.value =value;
+        public Node(int value) {
+            this.value = value;
         }
-        public Node(int value, Node next){
+
+        public Node(int value, Node next) {
             this.value = value;
             this.next = next;
-            
+
         }
     }
 }
+
